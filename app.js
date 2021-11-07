687b239c5af116d45ce1646e18900a1b09a71877
Alex Storm <als@edora.dk>
2021-11-07 17:57:23 +0100

    Implemented logic and SpecialAttack

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const app = Vue.createApp({
       playerArmorClass: 13,
       diceOutput: 0,
       damageOutput: 0,
+      critical: false,
+      currentRound: 0,
     };
   },
   computed: {
@@ -16,37 +18,54 @@ const app = Vue.createApp({
     playerBarStyles() {
       return { width: this.playerHealth + '%' };
     },
+    mayUseSpecialAttack() {
+      return this.currentRound % 3 !== 0;
+    },
   },
   methods: {
     attackMonster(num, dice, attNum, attDice) {
       this.diceOutput = this.diceRoll(num, dice);
+      this.currentRound++;
       if (this.diceOutput >= this.monsterArmorClass) {
-        console.log('You hit the monster!');
+        console.log('You hit the monster! You rolled ', this.diceOutput);
         this.monsterHealth -= this.damageRoll(attNum, attDice);
         console.log('MonsterHP: ', this.monsterHealth);
       } else {
-        console.log('You missed the monster!');
+        console.log('You missed the monster! You rolled ', this.diceOutput);
       }
       this.attackPlayer();
     },
     attackPlayer() {
       this.diceOutput = this.diceRoll(1, 20);
       if (this.diceOutput >= this.playerArmorClass) {
-        console.log('Monster hits you with an attack!');
+        console.log(
+          'Monster hits you with an attack! You rolled ',
+          this.diceOutput
+        );
         this.playerHealth -= this.damageRoll(1, 12);
         console.log('Player HP: ', this.playerHealth);
       } else {
-        console.log('The monster misses!');
+        console.log('The monster misses! You rolled ', this.diceOutput);
       }
     },
     diceRoll(num, dice) {
       if (dice) {
+        if (this.diceOutput === 20) {
+          console.log('You scored a critical!');
+          this.critical = true;
+        } else {
+          this.critical = false;
+        }
         return (this.diceOutput = num * Math.round(Math.random(1) * dice) + 1);
       }
     },
     damageRoll(num, dice) {
       if (dice) {
-        return (this.diceOutput = num * Math.round(Math.random(1) * dice));
+        if (this.critical === true) {
+          this.diceOutput = num * 2 * Math.round(Math.random(1) * dice);
+        } else {
+          return (this.diceOutput = num * Math.round(Math.random(1) * dice));
+        }
       }
     },
   },
